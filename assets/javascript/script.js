9bacbe7268ff7e448be214b6fbcf1da2093c3ad6
Cmoney <cascherer42@gmail.com>
2018-08-15 08:21:59 -0500

    Initial files, added firebase. Removed API key to not get error message

---
+++ assets/javascript/script.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  let config = {
+    authDomain: "cas-rps.firebaseapp.com",
+    databaseURL: "https://cas-rps.firebaseio.com",
+    projectId: "cas-rps",
+    storageBucket: "",
+    messagingSenderId: "265721348997"
+  };
+
+  firebase.initializeApp(config);
